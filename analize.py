5b8a6236964b9be06354ace5861fb58365477205
RenceRence <149395768+RenceRence@users.noreply.github.com>
2023-11-13 11:54:57 +0200

    Grafikas

--- analize.py
+++ analize.py
@@ -26,5 +26,23 @@ def pagal_savivaldybes():
     plt.tight_layout()
     plt.show()
 
-pagal_savivaldybes()
 
+
+#Profesinės ligos ir jų dažnis
+
+def profesiniu_lig_daz(year):
+    year =year
+    prof_lig_dazn_per_pas_met = Asmuo[Asmuo['year'] == year].groupby('priezasties_pav')['savivaldybe'].count()
+    prof_lig_dazn_per_pas_met = prof_lig_dazn_per_pas_met[prof_lig_dazn_per_pas_met > 10]
+    colors = ["lightskyblue", "lightcoral", "yellowgreen", "pink", "lightgreen", "purple"]
+    plt.pie(prof_lig_dazn_per_pas_met, labels=prof_lig_dazn_per_pas_met.index, autopct='%1.1f%%', startangle=90,
+            colors=colors)
+    plt.title("Profesinės ligos pagal priežastis ")
+    plt.savefig("profesines_lig_pagal_priez.png")
+    plt.show()
+
+
+
+
+# pagal_savivaldybes()
+# profesiniu_lig_daz(2022)
