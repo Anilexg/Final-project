8b8da9d134d4d274c23cd8fe834dd30b9dd05a96
Anilexg <149396634+Anilexg@users.noreply.github.com>
2023-11-13 16:42:46 +0200

    Atnaujintas analizė failas

--- analize.py
+++ analize.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import seaborn as sns
+
 from duomenys import get_data
 import matplotlib.pyplot as plt
 import requests
@@ -22,7 +23,7 @@ def pagal_savivaldybes():
     plt.ylabel('Atvejų_skaičius')
     plt.xticks(rotation=25,ha='right')
     plt.legend([])
-    plt.savefig("pagal_savivaldybes_2022.png")
+    plt.savefig("Pictures\pagal_savivaldybes_2022.png")
     plt.tight_layout()
     plt.show()
 
@@ -36,7 +37,7 @@ def profesiniu_lig_daz(year):
     plt.pie(prof_lig_dazn_per_pas_met, labels=prof_lig_dazn_per_pas_met.index, autopct='%1.1f%%', startangle=90,
             colors=colors,textprops={'fontsize': 6})
     plt.title(f'{year} metų\n' "Profesinės ligos pagal priežastis ")
-    plt.savefig("profesines_lig_pagal_priez.png")
+    plt.savefig("Pictures\profesines_lig_pagal_priez.png")
     plt.show()
 
 #Profesinių ligų pasiskirstymas pagal lytį
@@ -53,7 +54,7 @@ def lytis():
     plt.xticks(rotation=25, ha='right')
     plt.legend(title='Lytis', loc='upper right')
     plt.tight_layout()
-    plt.savefig("lytis_linijinis.png")
+    plt.savefig("Pictures\lytis_linijinis.png")
     plt.show()
 
 #Profesinių ligų skaičiaus kitimas savivaldybėse pasirinktų metų intervale
@@ -77,60 +78,10 @@ def kitimas_metais():
     plt.xticks(rotation=45, ha='right')
     plt.legend(title='Savivaldybe', bbox_to_anchor=(1.05, 1), loc='upper left')
     plt.tight_layout()
-    plt.savefig("pokytis_.png")
+    plt.savefig("Pictures\pokytis.png")
     plt.show()
 
-#
-def regresija():
-    year_start = 2013
-    year_end = 2023
-    filtruoti_duomenys = Asmuo[Asmuo['year'].between(year_start, year_end)]
-
-    rezultatas = filtruoti_duomenys.groupby(['savivaldybe', 'year']).size().unstack(fill_value=0)
-
-    rezultatas_per_metus_savivaldybe = rezultatas.sum(axis=1)
-
-    filtruota_savivaldybes = rezultatas_per_metus_savivaldybe[rezultatas_per_metus_savivaldybe > 0].index
-
-    filtruoti_rezultatas = rezultatas.loc[filtruota_savivaldybes]
-
-    filtruoti_rezultatas_ilgis = filtruoti_rezultatas.reset_index().melt(id_vars='savivaldybe', var_name='year',
-                                                                         value_name='count')
-    filtruoti_rezultatas_ilgis['year'] = pd.to_numeric(filtruoti_rezultatas_ilgis['year'], errors='coerce')
-
-    filtruoti_rezultatas_ilgis = filtruoti_rezultatas_ilgis.dropna(subset=['year'])
-
-    filtruoti_rezultatas_ilgis['year'] = filtruoti_rezultatas_ilgis['year'].astype(int)
-
-    plt.figure(figsize=(15, 7))
-    plt.title('Tendencijų linijos pagal Savivaldybe ir metus')
-    plt.xlabel('Metai')
-    plt.ylabel('Skaičius')
-    plt.xticks(rotation=45, ha='right')
-
-    slopes = {}
-
-    for name, group in filtruoti_rezultatas_ilgis.groupby('savivaldybe'):
-        slope, intercept, r_value, p_value, std_err = linregress(x=group['year'], y=group['count'])
-        slopes[name] = slope
-    for name, group in filtruoti_rezultatas_ilgis.groupby('savivaldybe'):
-        if slopes[name] > 0:
-            sns.regplot(
-                x='year', y='count', data=group,
-                label=name, scatter_kws={'s': 50},
-                line_kws={'linewidth': 2}, ci=None
-            )
-    plt.legend(title='Savivaldybe', bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.tight_layout()
-    plt.savefig("regresija.png")
-    plt.show()
-
-
-
-
-
+# kitimas_metais()
 # pagal_savivaldybes()
 # profesiniu_lig_daz(2020)
 # lytis()
-# kitimas_metais()
-# regresija()
