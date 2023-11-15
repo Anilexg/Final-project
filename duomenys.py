ddbab88c3ce9adb6092cd77c5606a85b2c8bc2fd
RenceRence <149395768+RenceRence@users.noreply.github.com>
2023-11-15 13:37:41 +0200

    atnaujinti failai

--- duomenys.py
+++ duomenys.py
@@ -1,26 +1,39 @@
 import pandas as pd
 # from web_scrap import scrape_table
 #
-#
+# # Kreipiamės į funkciją iš web_scrap.py, kad gautime duomenis iš interneto puslapio lentelės
 # asmuo = scrape_table()
+#
+# # Gauti duomenys keičiami į Pandas DataFrame su nurodytomis stulpelių pavadinimais
 # asmuo = pd.DataFrame(asmuo,columns=['id','m_id','pagr_tlk_kodas._id','pagr_tlk_pav._id','papild_tlk_kodas._id',
 #                                     'papild_tlk_pav._id','ligos_eiga','itarta_pav','itarta_kodas','priezasties_kodas',
 #                                     'priezasties_pav','lyties_kodas','lyties_pavadinimas','savivaldybe','year','quarter'])
 #
+# # Išsaugome duomenis į CSV failą, nurodytu pavadinimu ir nurodytoje vietoje
 # asmuo.to_csv("CSV file\ asmenys.csv", index=False)
-#
+# # Atsispausdiname
+# print(asmuo)
+
 # Kadangi interneto svetaineje yra apribojimas iki 100 eilučių, mūsų pasirinktas variantas netenkina, todėl tenka kreiptis į
 # duomenu bazeje pateiktus csv failus, kad analizė būtų vaizdingesnė ir pilnesnė.
+
+# Duomenų gavimas iš internetinio šaltinio
 def get_data():
+
+    # Nurodme interneto puslapio URL, iš kurio imsime duomenis
     url = "https://get.data.gov.lt/datasets/gov/hi/profesines/Asmuo/:format/csv"
+
+    # Perkeliame duomenis iš CSV formato internetinio šaltinio į Pandas DataFrame.
     Asmuo = pd.read_csv(url)
+
+    # Iš DataFrame pašaliname mums nereikalingus stulpelius
     Asmuo = Asmuo.drop(
         ['_type', '_id', '_revision', 'm_id', 'pagr_tlk_kodas._id', 'pagr_tlk_pav._id', 'papild_tlk_kodas._id',
          'papild_tlk_pav._id', 'ligos_eiga', 'itarta_pav', 'itarta_kodas', 'priezasties_kodas', 'quarter'], axis=1)
 
+    # Grąžina duomenis
     return Asmuo
 
-
+# Išsaugome duomenis į CSV failą, nurodytu pavadinimu ir nurodytoje vietoje
 # get_data().to_csv("CSV file\ Asmuo_new.csv", index=False)
-#
-# print(get_data().dtypes)
+
--- web_scrap.py
+++ web_scrap.py
@@ -3,25 +3,48 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-
+# Apsirašome draiverių inicializavimą
 service = Service(ChromeDriverManager().install())
+
+#Atidarome naują Chrome naršyklės langą, kuris valdomas Selenium biblioteka
 driver = webdriver.Chrome(service=service)
 driver.implicitly_wait(10)
 
+# Funkcija išgauname duomenis iš internetinio puslapio
 def scrape_table():
+    # Nurodme interneto puslapio URL, iš kurio imsime duomenis
     url="https://get.data.gov.lt/datasets/gov/hi/profesines/Asmuo"
+
+    # Atidaro nurodytą URL Chrome naršyklėje
     driver.get(url)
 
+    # Ieškome <table> elemento
     tbody = driver.find_element(By.XPATH, '//html/body/div[2]/table')
 
+    # Sukuriame tuščią sąrašą, kuriame bus saugomi gaunami duomenys
     data = []
+
+
+    # Ciklas kiekvienai lentelės eilutei (tr) ieško duomenų. Išskyrus antraštinę eilutę [1:]
+    # Ciklas suranda visas lentelės (tbody) eilutes naudodamas XPath.
     for tr in tbody.find_elements(By.XPATH, '//tr')[1:]:
+
+        # Kiekvienai eilutei (tr) suranda visus langelių elementus (td) ir išgauna teksto reikšmes
         row = [item.text for item in tr.find_elements(By.XPATH, './/td')]
+
+        # Rastos teksto duomenų eilutė pridedamos prie sąrašo
         data.append(row)
 
+
+    # Grąžina gautus duomenis
     return data
-    driver.quit()
 
-
-
+# Kreipimasis į funkciją
 scrape_table()
+
+
+
+
+
+
+
